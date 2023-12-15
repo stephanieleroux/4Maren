fdf8553d4eb5dca70dfbc03de5d43095d6d6bd18
Stephanie Leroux <stephanieleroux@users.noreply.github.com>
2023-12-15 14:49:09 +0100

    Update get_buoy_data-auto_SLX.sh

--- NOTEBOOKS/get_buoy_data-auto_SLX.sh
+++ NOTEBOOKS/get_buoy_data-auto_SLX.sh
@@ -49,3 +49,113 @@ WDIR=${tmpDIR}/working_${runDate}${pastDStr}
 buoyDIR=${WDIR}/buoy_data
 outputfile=sidfexloc
 mkdir -p ${WDIR} ${buoyDIR}
+
+# SLX
+# erase file if already exist
+if [ -f "${buoyDIR}/${outputfile}_${analDate}.dat" ]; then
+rm -f "${buoyDIR}/${outputfile}_${analDate}.dat"
+fi
+# create empty one
+touch "${buoyDIR}/${outputfile}_${analDate}.dat"
+
+############################################
+# create buoy list from AWI file
+#
+buoyList=${buoyDIR}/buoy_list.txt
+cd ${buoyDIR}
+echo -e "\nDownloading buoy target details from AWI website...\n"
+wget https://swift.dkrz.de/v1/dkrz_0262ea1f00e34439850f3f1d71817205/SIDFEx_index/SIDFEx_targettable.txt
+cat SIDFEx_targettable.txt | grep "^[0-9]" > ${buoyList}_tmp
+[[ -f ${buoyList} ]] && rm ${buoyList}
+
+# loop over buoy lines and decide if valid
+while read line; do
+   # extract buoy details from line
+   buoyID=$( echo ${line} | cut -d " " -f 1 )
+   buoyStartYEAR=$( echo ${line} | cut -d " " -f 9 )
+   buoyStartDAY=$( echo ${line} | cut -d " " -f 10 )
+   buoyEndYEAR=$( echo ${line} | cut -d " " -f 11 )
+   buoyEndDAY=$( echo ${line} | cut -d " " -f 12 )
+
+   # convert day of year to real buoy start and end dates
+   # (NB. the division by 1 with "bc" is the same as a "floor" operation)
+   buoyStart=$( date --date="${buoyStartYEAR}-01-01 +$(echo ${buoyStartDAY}/1 | bc) days" +"%Y%m%d" )
+   if [[ ${buoyEndYEAR} != "NaN" && ${buoyEndDAY} != "NaN" ]] ; then
+      buoyEnd=$( date --date="${buoyEndYEAR}-01-01 +$(echo ${buoyEndDAY}/1 | bc) days" +"%Y%m%d" )
+   else
+      # dummy future date
+      buoyEnd=21000101
+   fi  
+
+   # if the buoy is valid for our current date then write to the buoy list
+   if [[ ${analDate} -ge ${buoyStart} ]] && [[ ${analDate} -le ${buoyEnd} ]] ; then
+      echo ${buoyID} >> ${buoyList}
+   fi  
+
+done < ${buoyList}_tmp
+rm ${buoyList}_tmp
+
+############################################
+# get buoy data
+#
+echo -e "\nDownloading buoy details from IABP website...\n"
+
+# loop over buoys from file list
+numBuoys=0
+while read buoyID; do 
+
+   # get data from IABP
+   wget http://iabp.apl.washington.edu/WebData/${buoyID}.dat
+
+   # strip off top line
+   sed 1d ${buoyID}.dat > tmpFile.txt
+   mv tmpFile.txt ${buoyID}.dat
+
+   # extract relevant date and find closest report to 0Z for lat/lon positions
+   # save to ascii file 
+   python ${scriptDIR}/derive_buoy_lat-lon_v1.py ${buoyID} ${analDate} > ${buoyID}_${analDate}.dat
+
+   # count number of succesful buoy (non-zero) files and remove zero files that have failed 
+   if [[ -s ${buoyID}_${analDate}.dat ]] ; then
+      let numBuoys=${numBuoys}+1
+      # SLX print in a single file all the successful active buoys with their ID 
+      read -r longitude latitude < "${buoyID}_${analDate}.dat"
+      echo -e "${buoyID} $longitude $latitude" >> "${buoyDIR}/${outputfile}_${analDate}.dat"
+
+else
+      rm ${buoyID}_${analDate}.dat
+   fi
+
+
+
+done < ${buoyList} # read central buoy list
+
+# complain if no valid buoy files counted for today
+if [[ ${numBuoys} -eq 0 ]] ; then
+
+   echo "**ERROR** no buoys found for this date"
+
+   # if today's date (likely crontab initiated) then send an email warning
+   if [[ ${runDate} == ${todayDate} ]] ; then
+
+      # define email details
+      subject="SIDFEx WARNING : 0 buoys for ${analDate}"
+      EMAIL_ID="YOUREMAIL"
+      textfile="${buoyDIR}/email_warning.txt"
+      cat << EOC > ${textfile}
+Hi,
+Your SIDFEx job could not find any buoys with reports for yesterday's analysis day ${analDate}
+Best
+
+EOC
+
+      # send the email
+      echo "Sending warning email to ${EMAIL_ID}"
+      /usr/local/bin/mutt -s "${subject}" -i ${textfile} -x -z -n ${EMAIL_ID} << END || status="failed"
+END
+
+   fi # today
+
+   exit 9
+
+fi # no buoys
